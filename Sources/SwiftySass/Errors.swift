62f597fd254f4436ba0ede679706176aab8950cb
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-04-06 12:50:41 -0400

    Implement public function that accepts a source string and returns a compiled string

---
+++ Sources/SwiftySass/Errors.swift
@@ -0,0 +1,30 @@
+public struct SassCompilerError: Error {
+	/// Expanded error message
+	///
+	/// Example value:
+	/// ```
+	/// Error: Undefined variable: "$notDefined".
+	///         on line 2:9 of stdin
+	/// >>  color: $notDefined;
+	///
+	///    --------^
+	/// ```
+	public let message: String
+	
+	/// Textual description of the error
+	///
+	/// Example value:
+	/// ```
+	/// Undefined variable: "$notDefined".
+	/// ```
+	public let issue: String
+	
+	/// File where the error occurred
+	public let filename: String
+	
+	/// Line position of the error in `filename`
+	public let line: Int
+	
+	/// Column position of the error in `filename`
+	public let column: Int
+}
--- Sources/SwiftySass/SwiftySass.swift
+++ Sources/SwiftySass/SwiftySass.swift
@@ -8,4 +8,25 @@ public struct SwiftySass {
 	public static var sassLanguageVersion: String {
 		return String(cString: libsass_language_version())
 	}
+	
+	public static func render(source: String) throws -> String {
+		let sassContext = sass_make_data_context(sass_copy_c_string(source))
+		defer {
+			sass_delete_data_context(sassContext)
+		}
+		
+		sass_compile_data_context(sassContext)
+		
+		guard sass_context_get_error_status(sassContext) == 0 else {
+			let message = String(cString: sass_context_get_error_message(sassContext))
+			let text = String(cString: sass_context_get_error_text(sassContext))
+			let file = String(cString: sass_context_get_error_file(sassContext))
+			let line = sass_context_get_error_line(sassContext)
+			let column = sass_context_get_error_column(sassContext)
+			
+			throw SassCompilerError(message: message, issue: text, filename: file, line: line, column: column)
+		}
+		
+		return String(cString: sass_context_get_output_string(sassContext))
+	}
 }
