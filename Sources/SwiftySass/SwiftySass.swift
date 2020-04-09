57f7724d64afb39ad467de09ee3fe00267b55b04
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-04-09 10:59:18 -0400

    Refactor the public facing API methods
    I decided to remove them from the 'namespace' struct because Swift's
    module system still allows access through the module namespace as
    `SwifySass.foo`. With the struct, it was actually possible to call
    `SwiftySass.SwiftySass.foo`, which is odd.

--- Sources/SwiftySass/SwiftySass.swift
+++ Sources/SwiftySass/SwiftySass.swift
@@ -1,27 +1,27 @@
 import CLibSass
 import Foundation
 
-public struct SwiftySass {
-	public static var libSassVersion: String {
-		return String(cString: libsass_version())
-	}
-	
-	public static var sassLanguageVersion: String {
-		return String(cString: libsass_language_version())
-	}
-	
-	public static func compile(source: String) throws -> String {
-		let context = LibSassDataContext(sourceString: source)
-		return try context.compile()
-	}
-	
-	public static func compile(filePath: String) throws -> String {
-		let context = LibSassFileContext(inputPath: filePath)
-		return try context.compile()
-	}
-	
-	public static func compile(fileURL: URL) throws -> String {
-		let path = fileURL.path
-		return try compile(filePath: path)
-	}
+/// The current version of the compiled LibSass library
+public var libSassVersion: String {
+	return String(cString: libsass_version())
+}
+
+/// The current version of the Sass language
+public var sassLanguageVersion: String {
+	return String(cString: libsass_language_version())
+}
+
+public func compileSass(source: String) throws -> String {
+	let context = LibSassDataContext(sourceString: source)
+	return try context.compile()
+}
+
+public func compileSass(filePath: String) throws -> String {
+	let context = LibSassFileContext(inputPath: filePath)
+	return try context.compile()
+}
+
+public func compileSass(fileURL: URL) throws -> String {
+	let path = fileURL.path
+	return try compileSass(filePath: path)
 }
