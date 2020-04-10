5f61dbeed642268ef9af85ed5bf935d04e3fcd5a
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-04-10 14:24:07 -0400

    Add precondition check to LibSassContext.compile()

--- Sources/SwiftySass/Wrappers/LibSassContext.swift
+++ Sources/SwiftySass/Wrappers/LibSassContext.swift
@@ -8,8 +8,6 @@ class LibSassContext {
 	}
 	
 	func compile() throws -> String {
-		// Make sure that one of the compiler functions is called first
-		
 		guard sass_context_get_error_status(context) == 0 else {
 			throw SassCompilerError(
 				message: String(cString: sass_context_get_error_message(context)),
@@ -17,6 +15,11 @@ class LibSassContext {
 				filename: String(cString: sass_context_get_error_file(context)),
 				line: sass_context_get_error_line(context),
 				column: sass_context_get_error_column(context))
+		}
+		
+		// Make sure that one of the compile functions is called first
+		guard sass_context_get_output_string(context) != nil else {
+			preconditionFailure("Failed to call a libass compile function before calling super.compile()")
 		}
 		
 		return String(cString: sass_context_get_output_string(context))
