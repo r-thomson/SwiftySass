4e880df5cf6c63abe5b832720af25f6dc64a0737
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-04-18 11:41:05 -0400

    Add documentation comments to public functions

--- Sources/SwiftySass/SwiftySass.swift
+++ Sources/SwiftySass/SwiftySass.swift
@@ -11,11 +11,19 @@ public var sassLanguageVersion: String {
 	return String(cString: libsass_language_version())
 }
 
+/// Compiles the given source String and returns the compiled output as a String.
+/// - Parameter source: Sass source code to compile.
+/// - Throws: `SassCompilerError` if there is an error when compiling.
+/// - Returns: The compiled CSS as a String.
 public func compileSass(source: String) throws -> String {
 	let context = LibSassDataContext(sourceString: source)
 	return try context.compile()
 }
 
+/// Compiles the file at URL and returns the compiled output as a String.
+/// - Parameter fileURL: Sass source code to compile.
+/// - Throws: `SassCompilerError` if there is an error when compiling.
+/// - Returns: The compiled CSS as a String.
 public func compileSass(fileURL: URL) throws -> String {
 	let context = LibSassFileContext(inputPath: fileURL.path)
 	return try context.compile()
